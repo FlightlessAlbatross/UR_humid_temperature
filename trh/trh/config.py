4c2e4b5a87a221819769fd08883588c23d93e4d7
Martin <hofer@iiasa.ac.at>
2025-01-09 17:14:23 +0100

    add geolocations path

--- trh/trh/config.py
+++ trh/trh/config.py
@@ -14,3 +14,4 @@ apiurl_base_url = "https://platform-urbanreleaf.iccs.gr/FROST-Server/v1.1"
 ## data
 api_response_observations_utrecht = data_dir/ "raw/observations/utrecht.json"
 api_response_sensors_utrecht = data_dir/ "raw/sensors/utrecht"
+api_response_observations_geolocation = data_dir /"raw/observations/geolocations.jsonl"
