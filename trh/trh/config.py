f5ffe7702199082d3b5a0235b0be881e669a0581
Martin <hofer@iiasa.ac.at>
2025-01-09 15:52:39 +0100

    Add api endpoints to config file

--- trh/trh/config.py
+++ trh/trh/config.py
@@ -6,3 +6,11 @@ from typing import Tuple
 
 package_dir = Path(os.path.dirname(os.path.dirname(__file__)))
 data_dir = package_dir.parent / 'data'
+
+## API Endpoints 
+apiurl_base_url = "https://platform-urbanreleaf.iccs.gr/FROST-Server/v1.1"
+
+
+## data
+api_response_observations_utrecht = data_dir/ "raw/observations/utrecht.json"
+api_response_sensors_utrecht = data_dir/ "raw/sensors/utrecht"
