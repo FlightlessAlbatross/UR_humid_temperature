c9acbf57fe76790992c97fbe472ad473470364d5
Martin <hofer@iiasa.ac.at>
2025-01-16 20:15:24 +0100

    bugfixes / finishing preliminary code. It works now and runs through!
    Simplfied the output.
    Now I save the responses as is.
    Before I was selecting and formating already.
    
    A different (new) script will take over the selecting and formatting.

--- trh/trh/get_gps_by_sensor.py
+++ trh/trh/get_gps_by_sensor.py
@@ -5,6 +5,10 @@ from trh import api_client
 import time
 from datetime import datetime
 
+# if not os.path.exists(api_response_geolocations):
+#     with open(api_response_geolocations, 'w') as empty_file:
+#         pass
+os.makedirs(os.path.dirname(api_response_geolocations), exist_ok=True)
 
 # Load all observations grouped by device ID. 
 # we don't really need the device ID here, but we will keep the structure. 
@@ -13,8 +17,6 @@ with open(api_response_obs_by_sensor, 'r') as jsonl_file:
         for line in jsonl_file:
             try:
                 entry = json.loads(line)
-                
-                
                 if entry['device_id'] in observations_by_device:
                     raise ValueError("This is about to overwrite an existing entry that should not be here.")
                 
@@ -37,47 +39,39 @@ if os.path.exists(api_response_geolocations):
                 continue  # Handle cases where the file might have partial/corrupt lines
 
 
-
-
 with open(api_response_geolocations, 'a') as jsonl_file:
     for device_id, entry in observations_by_device.items():
         
-        for gps_entry in entry["responses"]:
+        for gps_entry in entry:
             
             # Skip if this entry has already been processed
-            if gps_entry['gps_id'] in gps_ids:
+            iot_id = gps_entry['@iot.id']
+            if iot_id in gps_ids:
                 continue
             
-
-            phenomenon_time = entry["phenomenonTime"]
-            variable_type = entry["result"]["type"]
-            value = entry["result"]["measurement"]
-            feature_url = api_client.adjust_frost_url(entry["FeatureOfInterest@iot.navigationLink"])
+            # phenomenon_time = gps_entry["phenomenonTime"]
+            # variable_type = gps_entry["result"]["type"]
+            # value = gps_entry["result"]["measurement"]
+            feature_url = api_client.adjust_frost_url(gps_entry["FeatureOfInterest@iot.navigationLink"])
 
             # fetch the geolocations
             gps_response = api_client.make_api_request(feature_url)
-            # Extract location, altitude, and quality
-            location = gps_response["feature"]["coordinates"]  # [longitude, latitude]
-            altitude = gps_response["properties"].get("altitude", None)  # Default to None if not present
-            quality = gps_response["properties"].get("quality", None)  # Default to None if not present
+            # # Extract location, altitude, and quality
+            # location = gps_response["feature"]["coordinates"]  # [longitude, latitude]
+            # altitude = gps_response["properties"].get("altitude", None)  # Default to None if not present
+            # quality = gps_response["properties"].get("quality", None)  # Default to None if not present
 
-            out = {"device_id" : device_id,
-                "phenomenonTime": phenomenon_time,
-                "type": variable_type,
-                "value": value,
-                "geoloc_url": feature_url, 
-                "location_quality": quality, 
-                "geometry_altitude": altitude, 
-                "geometry": {"type": "Point", "coordinates": location}}
+            # out = {"device_id" : device_id,
+            #     "phenomenonTime": phenomenon_time,
+            #     "type": variable_type,
+            #     "value": value,
+            #     "geoloc_url": feature_url, 
+            #     "location_quality": quality, 
+            #     "geometry_altitude": altitude, 
+            #     "geometry": {"type": "Point", "coordinates": location}}
             
-            jsonl_file.write(json.dumps(out) + '\n')
-            if i % 100 == 0:
-                current_time = datetime.now()
-                print(
-                    f"Iteration {i} at: { current_time.strftime('%H:%M') }"
-                    )
-
-            gps_ids.add(gps_id)
+            jsonl_file.write(json.dumps(gps_response) + '\n')
+            gps_ids.add(iot_id)
 
 
 
